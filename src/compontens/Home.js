587801db77aa31b3d4f7a67c2e9b15073a9c7185
Patrick Holst <holst92@gmail.com>
2023-02-13 15:24:24 +0100

    added animations.js

--- src/compontens/Home.js
+++ src/compontens/Home.js
@@ -1,72 +1,11 @@
 import React, { Component } from "react";
-import { Container, Carousel } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container-md">
-        <Container>
-          <Carousel variant="dark">
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/800/400"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h2>Parental Guide</h2>
-                <p>
-                  Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus-
-                  ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut
-                  teollisuuden normaali testausteksti jo 1500-luvulta asti,
-                  jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen
-                  tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä
-                  vuosisataa, mutta myös loikan elektroniseen kirjoitukseen,
-                  jääden suurinpiirtein muuntamattomana.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/800/400"
-                alt="Second slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Make Calculations</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
-                  sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
-                  amet, consectetur adipiscing elit.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://picsum.photos/800/400"
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Compare Plans</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit. Se ei ole
-                  selvinnyt vain viittä vuosisataa, mutta myös loikan
-                  elektroniseen kirjoitukseen, jääden suurinpiirtein
-                  muuntamattomana.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </Container>
+        <Container></Container>
       </div>
     );
   }
--- src/compontens/NavbarComp.js
+++ src/compontens/NavbarComp.js
@@ -6,59 +6,50 @@ import CompareCalc from "./CompareCalc";
 import Home from "./Home";
 import Insurances from "./Insurances";
 import OptimizedPlan from "./OptimizedPlan";
-import styled from "styled-components";
 
-const Styles = styled.div`
-  .NavDropdown {
-    backgroundcolor: "#93D7E0";
-  }
-`;
-
+// föregående färg: #93D7E0
+// annan färg #00ABE4
+// style i navbar: Navbar style={{ backgroundColor: "#00ABE4" }}
 export default class NavbarComp extends Component {
   render() {
     return (
-      <Styles>
-        <Router>
-          <div>
-            <Navbar style={{ backgroundColor: "#93D7E0" }}>
-              <Container>
-                <Navbar.Brand as={Link} to={"/"}>
-                  Parental Guide
-                </Navbar.Brand>
-                <Nav className="me-center">
-                  <NavDropdown
-                    title="Calculations"
-                    id="navbarScrollingDropdown"
-                  >
-                    <NavDropdown.Item as={Link} to={"/calculations"}>
-                      Create Calculation
-                    </NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to={"/comparecalc"}>
-                      Compare Calculations
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item as={Link} to={"/optimizedplan"}>
-                      Create optimized plan
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link as={Link} to={"/insurances"}>
-                    Insurances
-                  </Nav.Link>
-                </Nav>
-              </Container>
-            </Navbar>
-          </div>
-          <div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/calculations" element={<Calculations />} />
-              <Route path="/comparecalc" element={<CompareCalc />} />
-              <Route path="/optimizedplan" element={<OptimizedPlan />} />
-              <Route path="/insurances" element={<Insurances />} />
-            </Routes>
-          </div>
-        </Router>
-      </Styles>
+      <Router>
+        <div>
+          <Navbar>
+            <Container>
+              <Navbar.Brand as={Link} to={"/"}>
+                ParentalGuide
+              </Navbar.Brand>
+              <Nav className="me-center">
+                <NavDropdown title="Calculations" id="navbarScrollingDropdown">
+                  <NavDropdown.Item as={Link} to={"/calculations"}>
+                    Create Calculation
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/comparecalc"}>
+                    Compare Calculations
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to={"/optimizedplan"}>
+                    Create optimized plan
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link as={Link} to={"/insurances"}>
+                  Insurances
+                </Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculations" element={<Calculations />} />
+            <Route path="/comparecalc" element={<CompareCalc />} />
+            <Route path="/optimizedplan" element={<OptimizedPlan />} />
+            <Route path="/insurances" element={<Insurances />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
